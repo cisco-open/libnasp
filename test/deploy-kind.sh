36fced7a48fa7e5aaa030239b171ff63e5aba256
Nándor István Krácser <bonifaido@gmail.com>
2022-10-23 09:54:00 +0200

    create kind deployment (#9)
    * create kind deployment
    
    * fix icp ports to kind
    
    * make kind setup work on macos
    
    * add echo service for testing
    
    * add service,serviceaccount to sample client db
    
    * expose heimdall
    
    Co-authored-by: Zsolt Varga <zsolt.varga@console.hu>

---
+++ test/deploy-kind.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function log() {
+    echo -e "\n>>> ${1}\n"
+}
+
+DIRECTORY=`dirname $(readlink -f $0)`
+
+log "creating kind cluster"
+kind create cluster --wait 5m --config ${DIRECTORY}/kind.yaml
+
+log "setup and update helm repositories"
+helm repo add metallb https://metallb.github.io/metallb
+helm repo add banzaicloud-stable https://kubernetes-charts.banzaicloud.com
+helm repo update
+
+log "install metallb"
+helm install -n metallb-system --create-namespace metallb metallb/metallb --wait
+kubectl apply -f ${DIRECTORY}/metallb-config.yaml
+
+log "install istio"
+helm install --create-namespace --namespace=istio-system istio-operator banzaicloud-stable/istio-operator --wait
+kubectl apply --namespace istio-system -f ${DIRECTORY}/istio-controlplane.yaml
+
+log "waiting for istio controlplane to be available"
+kubectl wait --namespace istio-system \
+                --for=jsonpath='{.status.status}'="Available" \
+                --timeout=120s \
+                icp/icp-v115x
+
+log "install heimdall"
+kubectl create namespace heimdall
+kubectl label namespace heimdall istio.io/rev=icp-v115x.istio-system
+helm install -n heimdall heimdall ${DIRECTORY}/../experimental/heimdall/charts/heimdall --wait --set istio.meshGateway.enabled=true --set istio.meshGateway.nodePort=16443
+
+log "install echo service for testing"
+kubectl create namespace testing
+kubectl label namespace testing istio.io/rev=icp-v115x.istio-system
+kubectl apply --namespace testing -f ${DIRECTORY}/echo-service.yaml
