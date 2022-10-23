09b808ef35f4268f690ccb8c15db1b6e5f3c525b
Zsolt Varga <zsolt.varga@console.hu>
2022-10-23 13:27:33 +0200

    make kind deploy rerunnable and use local heimdall built image

--- test/deploy-kind.sh
+++ test/deploy-kind.sh
@@ -9,7 +9,9 @@ function log() {
 DIRECTORY=`dirname $(readlink -f $0)`
 
 log "creating kind cluster"
-kind create cluster --wait 5m --config ${DIRECTORY}/kind.yaml
+if ! $(kind create cluster --wait 5m --config ${DIRECTORY}/kind.yaml); then
+    echo ""
+fi
 
 log "setup and update helm repositories"
 helm repo add metallb https://metallb.github.io/metallb
@@ -17,11 +19,11 @@ helm repo add banzaicloud-stable https://kubernetes-charts.banzaicloud.com
 helm repo update
 
 log "install metallb"
-helm install -n metallb-system --create-namespace metallb metallb/metallb --wait
+helm upgrade --install -n metallb-system --create-namespace metallb metallb/metallb --wait
 kubectl apply -f ${DIRECTORY}/metallb-config.yaml
 
 log "install istio"
-helm install --create-namespace --namespace=istio-system istio-operator banzaicloud-stable/istio-operator --wait
+helm upgrade --install --create-namespace --namespace=istio-system istio-operator banzaicloud-stable/istio-operator --wait
 kubectl apply --namespace istio-system -f ${DIRECTORY}/istio-controlplane.yaml
 
 log "waiting for istio controlplane to be available"
@@ -37,12 +39,19 @@ kubectl wait --namespace istio-system \
                 --timeout=120s \
                 icp/icp-v115x
 
+log "build and load heimdall image"
+${DIRECTORY}/build-heimdall-image.sh
+
 log "install heimdall"
+if ! kubectl get namespace heimdall >/dev/null 2>&1; then
 kubectl create namespace heimdall
-kubectl label namespace heimdall istio.io/rev=icp-v115x.istio-system
-helm install -n heimdall heimdall ${DIRECTORY}/../experimental/heimdall/charts/heimdall --wait --set istio.meshGateway.enabled=true --set istio.meshGateway.nodePort=16443
+fi
+kubectl label namespace heimdall istio.io/rev=icp-v115x.istio-system --overwrite
+helm upgrade --install -n heimdall heimdall ${DIRECTORY}/../experimental/heimdall/charts/heimdall --wait --values ${DIRECTORY}/heimdall-values.yaml
 
 log "install echo service for testing"
+if ! kubectl get namespace testing >/dev/null 2>&1; then
 kubectl create namespace testing
+fi
 kubectl label namespace testing istio.io/rev=icp-v115x.istio-system
 kubectl apply --namespace testing -f ${DIRECTORY}/echo-service.yaml
