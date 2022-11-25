15e2fdec6954862b88f26971fdd7725edec848d3
Nandor Kracser <bonifaido@gmail.com>
2022-11-25 09:40:40 +0100

    wip

--- experimental/spring/spring-nasp-example/src/main/java/example/Application.java
+++ experimental/spring/spring-nasp-example/src/main/java/example/Application.java
@@ -7,12 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {NaspConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+
     @Bean
     public ReactiveWebServerFactory webServerFactory(NaspConfiguration configuration) {
         return new NaspWebServerFactory(configuration);
