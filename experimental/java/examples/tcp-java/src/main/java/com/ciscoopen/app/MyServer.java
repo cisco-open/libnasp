5c85abca0e2f83ce4734040a272e7adb10d78ebf
Balint Molnar <balintmolnar91@gmail.com>
2022-11-28 12:32:54 +0100

    Initial commit for java Nasp tcp client

--- experimental/java/examples/tcp-java/src/main/java/com/ciscoopen/app/MyServer.java
+++ experimental/java/examples/tcp-java/src/main/java/com/ciscoopen/app/MyServer.java
@@ -1,21 +1,47 @@
 package com.ciscoopen.app;
 
 import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class MyServer {
     public static void main(String[] args) {
-        initializeServer();
+//        initializeServer();
+        initializeClient();
+    }
+    public static void initializeClient() {
+        try (WrappedClientSocket wrappedSocket = new WrappedClientSocket()) {
+            wrappedSocket.connect(new InetSocketAddress("localhost", 10000));
+            InputStream input = wrappedSocket.getInputStream();
+            OutputStream output = wrappedSocket.getOutputStream();
+
+            Scanner scanner  = new Scanner(input);
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(output), true);
+            String message = "hello";
+            writer.println(message);
+            System.out.println("Request: " + message);
+            if (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println("Received: " + line);
+            }
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void initializeServer() {
-        try (WrappedServerSocket serverSocket = new WrappedServerSocket(9090)) {
+        try (WrappedServerSocket serverSocket = new WrappedServerSocket(10000)) {
             WrappedSocket connectionSocket = (WrappedSocket) serverSocket.accept();
             WrappedInputStream inputToServer = (WrappedInputStream) connectionSocket.getInputStream();
             WrappedOutputStream outputFromServer = (WrappedOutputStream) connectionSocket.getOutputStream();
             
-            Scanner scanner = new Scanner(inputToServer, "UTF-8");
-            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
+            Scanner scanner = new Scanner(inputToServer);
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer), true);
 
             boolean done = false;
 
