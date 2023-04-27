bf9d0ac45bb8a986b0f8c16f295d39b0881f5f04
Balint Molnar <balintmolnar91@gmail.com>
2023-04-27 11:35:25 +0200

    Java side nio improvements

--- experimental/java/examples/nio-java/src/main/java/com/ciscoopen/app/NaspSelectorProvider.java
+++ experimental/java/examples/nio-java/src/main/java/com/ciscoopen/app/NaspSelectorProvider.java
@@ -92,18 +92,24 @@ class NaspSelector extends SelectorImpl {
 
     private void updateRunningAsyncOps(int selectedKeyId, int updateOps) {
         int runningOps = 0;
-        if (runningAsyncOps.containsKey(selectedKeyId)) {
-            runningOps = runningAsyncOps.get(selectedKeyId);
-        }
+        runningOps = runningAsyncOps.get(selectedKeyId);
         if ((updateOps & SelectionKey.OP_READ) != 0) {
-            runningAsyncOps.put(selectedKeyId, runningOps | SelectionKey.OP_READ);
+            if ((runningOps & SelectionKey.OP_READ) == 0) {
+                runningAsyncOps.put(selectedKeyId, runningOps | SelectionKey.OP_READ);
+            }
         } else {
-            runningAsyncOps.put(selectedKeyId, runningOps & ~SelectionKey.OP_READ);
+            if ((runningOps & SelectionKey.OP_READ) != 0) {
+                runningAsyncOps.put(selectedKeyId, runningOps & ~SelectionKey.OP_READ);
+            }
         }
         if ((updateOps & SelectionKey.OP_WRITE) != 0) {
-            runningAsyncOps.put(selectedKeyId, runningOps | SelectionKey.OP_WRITE);
+            if ((runningOps & SelectionKey.OP_WRITE) == 0) {
+                runningAsyncOps.put(selectedKeyId, runningOps | SelectionKey.OP_WRITE);
+            }
         } else {
-            runningAsyncOps.put(selectedKeyId, runningOps & ~SelectionKey.OP_WRITE);
+            if ((runningOps & SelectionKey.OP_WRITE) != 0) {
+                runningAsyncOps.put(selectedKeyId, runningOps & ~SelectionKey.OP_WRITE);
+            }
         }
     }
 
